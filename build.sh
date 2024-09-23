c3289b136511c257150e69b9086b5dab202880c1
Peter Hirn <peter.hirn@phi.ag>
2024-09-23 15:40:51 +0200

    chore: reorder emcc arguments

--- build.sh
+++ build.sh
@@ -6,14 +6,13 @@ set -eux
 
   emcc -std=c89 -Wall -Wextra -Werror -Wno-type-limits \
     -O3 -msimd128 -msse4.2 -mavx -flto \
-    -Iinclude -Isrc \
     --cache .cache \
-    -DARGON2_NO_THREADS \
     -s NO_ASSERTIONS \
     -s WASM_BIGINT \
     -s NO_FILESYSTEM \
     -s MODULARIZE \
     -s EXPORT_ES6 \
+    -s NO_USE_ES6_IMPORT_META \
     -s EXPORT_NAME=Argon2ModuleFactory \
     -s EXPORTED_FUNCTIONS="_malloc","_free","_argon2_hash","_argon2_verify","_argon2_error_message","_argon2_encodedlen" \
     -s EXPORTED_RUNTIME_METHODS="UTF8ToString" \
@@ -21,7 +20,8 @@ set -eux
     -s INITIAL_HEAP=67108864 \
     -s ALLOW_MEMORY_GROWTH \
     -s ENVIRONMENT="web" \
-    -s NO_USE_ES6_IMPORT_META \
+    -D ARGON2_NO_THREADS \
+    -I include \
     src/argon2.c src/core.c src/blake2/blake2b.c src/encoding.c src/opt.c \
     -o argon2.js
 
