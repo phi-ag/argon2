9695e31a558696d2e23594ab163910d2ed97c0c5
Peter Hirn <peter.hirn@phi.ag>
2024-09-24 07:36:39 +0200

    chore: move emscripten cache

--- build.sh
+++ build.sh
@@ -1,33 +1,39 @@
 #!/usr/bin/env sh
-set -eux
+set -eu
 
 (
   cd phc-winner-argon2
 
+  emcc -v
+
+  # - Optimizations see https://emscripten.org/docs/optimizing/Optimizing-Code.html
+  # - SIMD see https://emscripten.org/docs/porting/simd.html
+  # - Compiler settings see https://emscripten.org/docs/tools_reference/settings_reference.html
   emcc -std=c89 -Wall -Wextra -Werror -Wno-type-limits \
-    -O3 -msimd128 -msse4.2 -mavx -flto \
-    --cache .cache \
-    -s NO_ASSERTIONS \
-    -s WASM_BIGINT \
-    -s NO_FILESYSTEM \
-    -s MODULARIZE \
-    -s EXPORT_ES6 \
-    -s NO_USE_ES6_IMPORT_META \
-    -s EXPORT_NAME=Argon2ModuleFactory \
-    -s EXPORTED_FUNCTIONS="_malloc","_free","_argon2_hash","_argon2_verify","_argon2_error_message","_argon2_encodedlen" \
-    -s EXPORTED_RUNTIME_METHODS="UTF8ToString" \
-    -s MALLOC=emmalloc \
-    -s INITIAL_HEAP=67174400 \
-    -s ALLOW_MEMORY_GROWTH \
-    -s ENVIRONMENT="web" \
-    -D ARGON2_NO_THREADS \
-    -I include \
-    src/argon2.c src/core.c src/blake2/blake2b.c src/encoding.c src/opt.c \
-    -o argon2.js
+    -O3 -flto -msimd128 -msse4.2 -mavx \
+    -sVERBOSE \
+    -sNO_ASSERTIONS \
+    -sWASM_BIGINT \
+    -sNO_FILESYSTEM \
+    -sMODULARIZE \
+    -sEXPORT_ES6 \
+    -sNO_USE_ES6_IMPORT_META \
+    -sEXPORT_NAME=Argon2ModuleFactory \
+    -sEXPORTED_FUNCTIONS=_malloc,_free,_argon2_hash,_argon2_verify,_argon2_error_message,_argon2_encodedlen \
+    -sEXPORTED_RUNTIME_METHODS=UTF8ToString \
+    -sMALLOC=emmalloc \
+    -sINITIAL_HEAP=67174400 \
+    -sALLOW_MEMORY_GROWTH \
+    -sENVIRONMENT="web" \
+    -DARGON2_NO_THREADS \
+    --cache=../.emscripten \
+    -Iinclude \
+    -o ../src/argon2.js \
+    src/argon2.c src/core.c src/blake2/blake2b.c src/encoding.c src/opt.c
 
-  shasum argon2.wasm
-  shasum argon2.js
+  shasum ../src/argon2.wasm
+  shasum ../src/argon2.js
 
-  cp -f argon2.wasm argon2.js ../src
-  ls -l ../src
+  stat -c "%n %s" ../src/argon2.wasm
+  stat -c "%n %s" ../src/argon2.js
 )
