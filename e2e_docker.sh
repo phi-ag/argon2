cfe3098f4d19f0e2406cfd5bc234fbc9151157dc
Peter Hirn <peter.hirn@phi.ag>
2025-05-25 09:04:22 +0200

    test: add e2e docker script

---
+++ e2e_docker.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+set -eu
+
+VERSION=v1.52.0-noble@sha256:a021500a801bab0611049217ffad6b9697d827205c15babb86a53bc1a61c02d5
+STORE_PATH="$(pnpm store path --silent)"
+
+mkdir -p .run-playwright
+
+cat <<EOF >.run-playwright/run.sh
+#!/usr/bin/env bash
+set -euo pipefail
+corepack enable
+corepack prepare --activate
+pnpm config set store-dir ${STORE_PATH}
+pnpm install --frozen-lockfile
+pnpm test:e2e
+EOF
+
+chmod +x .run-playwright/run.sh
+
+docker run -it --rm \
+  --workdir /workdir \
+  -v .:/workdir \
+  -v ${STORE_PATH}:${STORE_PATH} \
+  -e CI=true -e HOME=/root -e STORE_PATH -e BASE_URL \
+  mcr.microsoft.com/playwright:${VERSION} \
+  .run-playwright/run.sh
