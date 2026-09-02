3a4adf489248ecd650cd94b0f2ddc12edc30cca1
Peter Hirn <peter.hirn@phi.ag>
2026-09-02 10:12:55 +0200

    chore: remove .pnpmfile.cjs

--- .pnpmfile.cjs
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line no-undef
-module.exports = {
-  hooks: {
-    readPackage: (pkg) => {
-      if (pkg.name === "@cyclonedx/cyclonedx-library") delete pkg.optionalDependencies;
-      return pkg;
-    }
-  }
-};
